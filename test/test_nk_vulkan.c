d358e25ce6a7e603e9c4a238e4abe28b6a15a101
Xichen Zhou <xzhou@xeechou.net>
2019-05-02 10:45:37 -0400

    -a

--- test/test_nk_vulkan.c
+++ test/test_nk_vulkan.c
@@ -107,7 +107,9 @@ int main(int argc, char *argv[])
 	struct nk_wl_backend *backend = nk_vulkan_backend_create();
 	nk_vulkan_impl_app_surface(&App.surface, backend, sample_widget, 100, 200, 0, 0);
 
+	app_surface_release(&App.surface);
 	nk_vulkan_backend_destroy(backend);
+
 	free(backend);
 	return 0;
 }
