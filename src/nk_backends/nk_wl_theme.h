75c253937847a34296f708491bc0c457f159e1f9
Xichen Zhou <xzhou@xeechou.net>
2019-12-18 13:57:53 -0500

    licensed under gplv2

--- src/nk_backends/nk_wl_theme.h
+++ src/nk_backends/nk_wl_theme.h
@@ -1,3 +1,24 @@
+/*
+ * nk_wl_theme.h - taiwins client nuklear theme handling functions
+ *
+ * Copyright (c) 2019 Xichen Zhou
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 #ifndef NK_WL_THEME_H
 #define NK_WL_THEME_H
 
