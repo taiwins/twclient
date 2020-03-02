fa66395215bd0254a90961744d34f99423f90965
xzhou <xzhou@qube-4d.com>
2020-03-02 00:01:15 -0500

    add vulkan library in ci

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland libglvnd vulkan-headers wayland-protocols fontconfig freetype2 librsvg
+pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland libglvnd vulkan-headers vulkan-icd-loader wayland-protocols fontconfig freetype2 librsvg
 pacman -Syu --needed --noconfirm cmake make gcc clang musl git meson ninja rsync pkgconfig
 # build step
 mkdir -p build && cd build && cmake .. && make
