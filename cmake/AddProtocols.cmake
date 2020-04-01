1089c2116313aa35fb378fb2bfc8e80920fde402
Xichen Zhou <xzhou@xeechou.net>
2020-04-01 19:50:10 -0400

    twclient does not need taiwins-protocols

--- cmake/AddProtocols.cmake
+++ cmake/AddProtocols.cmake
@@ -3,20 +3,12 @@
 ################################################################################
 include(Wayland)
 
-set(TAIWINS_PROTOS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/protocols")
-
-WAYLAND_ADD_PROTOCOL_CLIENT(proto_twtheme_client
-  "${TAIWINS_PROTOS_DIR}/taiwins-theme.xml"
-  taiwins-theme
-  )
-
 #not able to utilse targets
 WAYLAND_ADD_PROTOCOL_CLIENT(proto_xdg_shell_client
   "${WLPROTO_PATH}/stable/xdg-shell/xdg-shell.xml"
   xdg-shell)
 
 add_library(twclient-protos STATIC
-  ${proto_twtheme_client}
   ${proto_xdg_shell_client}
   )
 
