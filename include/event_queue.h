e0adde8a777b7517a0f734a1bf1dc77e3547d05f
Xichen Zhou <xzhou@xeechou.net>
2020-04-03 20:48:56 -0400

    add tw_signal

--- include/event_queue.h
+++ include/event_queue.h
@@ -57,8 +57,11 @@ struct tw_event_queue {
 
 };
 
-void tw_event_queue_run(struct tw_event_queue *queue);
-bool tw_event_queue_init(struct tw_event_queue *queue);
+void
+tw_event_queue_run(struct tw_event_queue *queue);
+
+bool
+tw_event_queue_init(struct tw_event_queue *queue);
 
 
 /**
@@ -67,33 +70,38 @@ bool tw_event_queue_init(struct tw_event_queue *queue);
  * epoll fd are usually stream files, for normal files (except procfs and
  * sysfs), use `tw_event_queue_add_file`
  */
-int tw_event_queue_add_source(struct tw_event_queue *queue, int fd,
-                              struct tw_event *event, uint32_t mask);
+int
+tw_event_queue_add_source(struct tw_event_queue *queue, int fd,
+                          struct tw_event *event, uint32_t mask);
 
 /**
  * @brief remove items from event queue
  *
  */
-bool tw_event_queue_remove_source(struct tw_event_queue *queue, int fd);
+bool
+tw_event_queue_remove_source(struct tw_event_queue *queue, int fd);
 
-bool tw_event_queue_modify_source(struct tw_event_queue *queue, int fd,
-                                  struct tw_event *e, uint32_t mask);
+bool
+tw_event_queue_modify_source(struct tw_event_queue *queue, int fd,
+                             struct tw_event *e, uint32_t mask);
 
 /**
  * @brief add a file to inotify watch system
  *
  * this adds a inotify fd to event queue, this does not work for sysfs
  */
-int tw_event_queue_add_file(struct tw_event_queue *queue, const char *path,
-			     struct tw_event *e, uint32_t mask);
+int
+tw_event_queue_add_file(struct tw_event_queue *queue, const char *path,
+                        struct tw_event *e, uint32_t mask);
 
 /**
  * @brief add a device to udev monitoring system
  *
  * subsystem shouldn't be none.
  */
-int tw_event_queue_add_device(struct tw_event_queue *queue, const char *subsystem,
-                              const char *devname, struct tw_event *e);
+int
+tw_event_queue_add_device(struct tw_event_queue *queue, const char *subsystem,
+                          const char *devname, struct tw_event *e);
 
 /**
  * @brief returns an allocated udev_device
@@ -102,14 +110,18 @@ int tw_event_queue_add_device(struct tw_event_queue *queue, const char *subsyste
  * `udev_device_unref`. And you should only call this function with udev event,
  * otherwise the behavior is undefined
  */
-struct udev_device *tw_event_get_udev_device(struct tw_event_queue *queue, int fd);
+struct udev_device *
+tw_event_get_udev_device(struct tw_event_queue *queue, int fd);
 
-int tw_event_queue_add_timer(struct tw_event_queue *queue, const struct itimerspec *interval,
-			      struct tw_event *event);
-
-int tw_event_queue_add_wl_display(struct tw_event_queue *queue, struct wl_display *d);
-
-bool tw_event_queue_add_idle(struct tw_event_queue *queue, struct tw_event *e);
+int
+tw_event_queue_add_timer(struct tw_event_queue *queue,
+                         const struct itimerspec *interval,
+                         struct tw_event *event);
+int
+tw_event_queue_add_wl_display(struct tw_event_queue *queue,
+                              struct wl_display *d);
+bool
+tw_event_queue_add_idle(struct tw_event_queue *queue, struct tw_event *e);
 
 
 #ifdef __cplusplus
