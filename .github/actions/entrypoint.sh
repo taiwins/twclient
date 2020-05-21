8a81f757fbb4727e21c2d11d870392abc0c8cfcf
Xichen Zhou <xzhou@xeechou.net>
2020-05-20 23:07:04 -0400

    ci: switch to meson build
    Signed-off-by: Xichen Zhou <xzhou@xeechou.net>

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -3,4 +3,4 @@
 pacman -Syu --needed --noconfirm weston libxkbcommon pixman mesa wayland libglvnd vulkan-headers vulkan-icd-loader wayland-protocols fontconfig freetype2 librsvg
 pacman -Syu --needed --noconfirm cmake make gcc clang musl git meson ninja rsync pkgconfig
 # build step
-mkdir -p build && cd build && cmake .. && make
+meson build && ninja -C build
